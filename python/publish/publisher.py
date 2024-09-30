06fa526be7054eabec147eacc5a5f4d3328e7021
Frederik Jacobsen <Frederik1904@gmail.com>
2024-10-01 00:03:44 +0200

    Removed gh from init

--- python/publish/publisher.py
+++ python/publish/publisher.py
@@ -556,10 +556,10 @@ class Publisher:
 
         summary = get_long_summary_with_digest_md(stats_with_delta, stats, details_url, test_changes, self._settings.test_changes_limit)
         body = f'## {title}\n{summary}'
-
+        body_map = {body: body}
         # only create new comment none exists already
         if latest_comment is None:
-            comment = IssueApi(self._gtea).issue_create_comment(self._settings.repo.split("/")[0], self._settings.repo.split("/")[1], 2, body=body)
+            comment = IssueApi(self._gtea).issue_create_comment(self._settings.repo.split("/")[0], self._settings.repo.split("/")[1], 2, body=body_map)
             #comment = pull_request.create_issue_comment(body)
             logger.info(f'Created comment for pull request #{pull_request.number}: {comment.html_url}')
         else:
